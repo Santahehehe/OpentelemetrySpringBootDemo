ec7c9e7f903db9e3a24046f1c41b3b78f46163c0
samlj <samljin123@gmail.com>
2025-08-22 22:43:17 +0800

    讓opentelemetrydemo build出來的 war檔可以在外部的Tomcat部署

--- src/main/java/com/example/opentelemetrydemo/OpentelemetrydemoApplication.java
+++ src/main/java/com/example/opentelemetrydemo/OpentelemetrydemoApplication.java
@@ -2,12 +2,19 @@ package com.example.opentelemetrydemo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
-public class OpentelemetrydemoApplication {
+public class OpentelemetrydemoApplication extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(OpentelemetrydemoApplication.class);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(OpentelemetrydemoApplication.class, args);
